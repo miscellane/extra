0d53a9f54dd0a145ac96ce4eed460041d237ca76
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-08 07:03:28 +0100

    Add  src/main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import logging
+import os
+import sys
+
+
+def main():
+    logger.info('investments')
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    main()
