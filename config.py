92dc499a2308a17371ce0bfa346e2d256d4e6dc4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-08 20:09:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
 
     def __init__(self):
 
-        Expenditure = collections.namedtuple(typename='Expenditure', field_names=['exclude', 'years', 'datasets'])
+        Expenditure = collections.namedtuple(typename='Expenditure', field_names=['exclude', 'years', 'datapath'])
         start = 1996
         stop = 2021
         self.expenditure = Expenditure(
             exclude=['_T', 'GF01', 'GF02', 'GF03', 'GF04', 'GF05', 'GF06', 'GF07', 'GF08', 'GF09', 'GF10'],
             years=np.linspace(start=start, stop=stop, num=stop - start + 1, dtype=int),
-            datasets=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'initial'))
+            datapath=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'initial'))
