cce5f7fdb612180b93cf83a4da73fd8a8ee0031d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-08 10:15:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import collections
 
+import numpy as np
+
 
 class Config:
 
     def __init__(self):
-
-        Expenditure = collections.namedtuple(typename='Expenditure', field_names=['exclude'])
+        
+        Expenditure = collections.namedtuple(typename='Expenditure', field_names=['exclude', 'years'])
+        start = 1996
+        stop = 2021
         self.expenditure = Expenditure(
-            exclude=['_T', 'GF01', 'GF02', 'GF03', 'GF04', 'GF05', 'GF06', 'GF07', 'GF08', 'GF09', 'GF10'])
+            exclude=['_T', 'GF01', 'GF02', 'GF03', 'GF04', 'GF05', 'GF06', 'GF07', 'GF08', 'GF09', 'GF10'],
+            years=np.linspace(start=start, stop=stop, num=stop - start + 1))
