a01b0feff7b6b25aaa01993ed0da04c2750ef375
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-19 23:06:38 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,3 +1,7 @@
+"""
+interface.py
+"""
+import logging
 import os
 
 import src.algorithms.overall
@@ -7,12 +11,21 @@ import src.functions.directories
 
 
 class Interface:
+    """
+
+    """
 
     def __init__(self):
-        pass
+        """
 
-    @staticmethod
-    def graphs():
+        """
+
+        # Logging
+        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def graphs(self):
 
         path = os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'graphs')
 
@@ -21,5 +34,8 @@ class Interface:
         for pathstr in [path, os.path.join(path, 'excerpts')]:
             directories.create(path=pathstr)
 
-        src.algorithms.overall.Overall(storage=path).exc()
-        src.algorithms.excerpts.Excerpts(storage=os.path.join(path, 'excerpts')).exc()
+        message = src.algorithms.overall.Overall(storage=path).exc()
+        self.__logger.info(message)
+
+        message = src.algorithms.excerpts.Excerpts(storage=os.path.join(path, 'excerpts')).exc()
+        self.__logger.info(message)
