4e8aa91f93c79cdf600d7640b544b1a1a65ef50b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-21 18:02:31 +0100

    Add src/metrics/overall.py

---
+++ src/metrics/overall.py
@@ -0,0 +1,87 @@
+"""
+overall.py
+"""
+import os
+
+import dask
+import dask.dataframe
+import pandas as pd
+
+import config
+import src.adjust.transactions
+import src.functions.directories
+import src.functions.objects
+import src.functions.streams
+
+
+class Overall:
+    """
+    Overall
+    """
+
+    def __init__(self, storage: str):
+        """
+
+        :param storage:
+        """
+
+        # The resulting graphing data will be stored in ...
+        self.__storage = storage
+
+        # The overarching foci, i.e., segments, e.g., defence, economic affairs, etc.
+        self.__segments = src.adjust.transactions.Transactions().segments
+
+        # The calculations must be based on revalued data sets, hence comparable prices/costs across years.
+        self.__datapath = config.Config().expenditure.datapath
+
+        # The fields in focus: The overall government expenditure per segment code is recorded in field <OTE>
+        self.__usecols = ['code', 'OTE', 'segment_code', 'year', 'epoch']
+
+    def __read(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+
+        frame = dask.dataframe.read_csv(
+            urlpath=os.path.join(self.__datapath, '*.csv'), usecols=self.__usecols)
+        data = frame.compute().reset_index(drop=True)
+
+        return data
+
+    def __persist(self, dictionary) -> str:
+        """
+
+        :param dictionary:
+        :return:
+        """
+
+        return src.functions.objects.Objects().write(
+            nodes=dictionary, path=os.path.join(self.__storage, 'overall.json'))
+
+    @staticmethod
+    def __aggregates(blob: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param blob:
+        :return:
+        """
+
+        aggregates = blob.copy().groupby(by=['segment_code', 'epoch']).agg(total=('OTE', sum))
+        aggregates.reset_index(drop=False, inplace=True)
+
+        return aggregates
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        # Read-in the revalued data
+        data = self.__read()
+
+        # Get aggregates by segment
+        aggregates = self.__aggregates(blob=data)
+
+
