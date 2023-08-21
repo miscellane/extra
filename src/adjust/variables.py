e9dcb923567100af611ddc263dde3d539ce4fdc4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-21 13:28:33 +0100

    Update src/adjust/variables.py

--- src/adjust/variables.py
+++ src/adjust/variables.py
@@ -19,7 +19,7 @@ class Variables:
 
         """
 
-        self.__uri = os.path.join(os.getcwd(), 'data', 'variables.csv')
+        self.__uri = os.path.join(os.getcwd(), 'data', 'expenditure_variables.csv')
 
         # the variables data
         self.data: pd.DataFrame = self.__exc()
