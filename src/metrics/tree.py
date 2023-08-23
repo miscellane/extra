12073b599699f2bb3789874c31a85bcf774fa7ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-23 15:26:44 +0100

    Update src/metrics/tree.py

--- src/metrics/tree.py
+++ src/metrics/tree.py
@@ -1,73 +1,39 @@
 import os
 
 import dask
-import dask.dataframe
 import numpy as np
 import pandas as pd
 
-import config
+import src.metrics.vertex
 
 
-class Prototype:
+class Tree:
 
-    def __init__(self):
+    def __init__(self, data: pd.DataFrame, focus: str):
         """
 
+        :param data:
+        :param focus:
         """
 
-        # The calculations must be based on revalued data sets, hence comparable prices/costs across years.
-        self.__datapath = config.Config().expenditure.datapath
+        self.__fields = data.columns.drop(labels=['epoch'])
+        self.__vertex = src.metrics.vertex.Vertex(data=data, focus=focus)
 
-        # The fields in focus: The overall government expenditure per segment code is recorded in field <OTE>
-        self.__usecols = ['code', 'OTE', 'segment_code', 'year', 'epoch']
+    @dask.delayed
+    def __part(self, field: str) -> dict:
 
-    def __read(self) -> pd.DataFrame:
-        """
+        return self.__vertex.exc(part=field)
 
-        :return:
-        """
-
-        frame = dask.dataframe.read_csv(
-            urlpath=os.path.join(self.__datapath, '*.csv'), usecols=self.__usecols)
-        data = frame.compute().reset_index(drop=True)
-
-        return data
-
-    @staticmethod
-    def __aggregates(blob: pd.DataFrame) -> pd.DataFrame:
-        """
-
-        :param blob:
-        :return:
-        """
-
-        aggregates = blob.copy().groupby(by=['segment_code', 'epoch']).agg(annual_total=('OTE', sum))
-        aggregates.reset_index(drop=False, inplace=True)
-
-        return aggregates
-
-    def exc(self) -> pd.DataFrame:
+    def exc(self):
         """
 
         :return:
         """
 
-        # Read-in the revalued data
-        data = self.__read()
+        computations = []
+        for field in self.__fields:
+            part = self.__part(field=field)
+            computations.append(part)
+        dictionary = dask.compute(computations, scheduler='threads')[0]
 
-        # Per segment code, how much was spent each epoch year?
-        aggregates = self.__aggregates(blob=data)
-
-        # What is the expenditure per epoch year?
-        temporary = aggregates.groupby(by=['epoch']).agg(denominator=('annual_total', sum))
-        temporary.reset_index(drop=False, inplace=True)
-        temporary = aggregates.merge(temporary.copy(), how='left', on='epoch')
-        temporary.loc[:, 'annual_segment_%'] = 100 * temporary['annual_total'] / temporary['denominator']
-
-        # Per segment code time series, evaluate the delta percentage vis-à-vis the previous year
-        temporary.sort_values(by=['segment_code', 'epoch'], ascending=True, inplace=True)
-        temporary.loc[:, 'series_delta'] = temporary.groupby(by=['segment_code'])['annual_total'].diff().fillna(np.NaN)
-        temporary.loc[:, 'series_shift'] = temporary['annual_total'].shift(periods=1, fill_value=np.NaN)
-        temporary.loc[:, 'series_delta_%'] = 100 * temporary['series_delta'] / temporary['series_shift']
-
-        return temporary
+        return dictionary
