89960d9b502b268f4ae49c2f772ef955aafbece2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-23 12:44:36 +0100

    Add src/metrics/prototype.py

---
+++ src/metrics/prototype.py
@@ -0,0 +1,12 @@
+
+class Prototype:
+
+    def __init__(self):
+        pass
+
+    def exc(self):
+        """
+        Investigate an alternative graphing data structure for aggregates ...
+
+        :return:
+        """
