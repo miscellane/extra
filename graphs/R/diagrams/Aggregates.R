713864c4d75683681518d19c34b73b6c4af4c530
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-26 00:46:18 +0100

    Update graphs/R/diagrams/Aggregates.R

--- graphs/R/diagrams/Aggregates.R
+++ graphs/R/diagrams/Aggregates.R
@@ -7,6 +7,14 @@ Aggregates <- function () {
   T <- dplyr::rename(T, annual_segment_rate = 'annual_segment_.', series_delta_rate = 'series_delta_.')
   str(object = T)
 
+  structure <- T %>%
+    dplyr::select(segment_code, year, annual_segment_total, annual_segment_rate, series_delta_rate) %>%
+    tidyr::gather(key = 'partition', value = 'value', -c('segment_code', 'year'))
+  structure$partition <- factor(structure$partition,
+                                levels = c( 'annual_segment_total', 'annual_segment_rate', 'series_delta_rate'),
+                                labels = c('Segment Total (m£)', 'Segment (%)', 'Delta (%)'))
+  str(object = structure)
+
   descriptions <- data.table::fread(file = file.path(getwd(), 'data', 'expenditure', 'expenditure_transaction_types.csv'),
                                     select = c('segment_code', 'segment_description'), strip.white = TRUE,
                                     colClasses = c(segment_code = 'character', segment_description = 'character'),
@@ -14,16 +22,14 @@ Aggregates <- function () {
   descriptions
 
 
-  diagram <- T %>%
-    dplyr::select(segment_code, year, annual_segment_total, annual_segment_rate, series_delta_rate) %>%
-    tidyr::gather(key = 'partition', value = 'value', -c('segment_code', 'year')) %>%
+  diagram <- structure %>%
     ggplot(mapping = aes(x = year, y = value, colour = segment_code)) +
     geom_point(alpha = 0.25, na.rm = TRUE) +
     facet_wrap(~partition, nrow = 3, ncol = 1, scales = 'free_y') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 2, units = 'lines'),
           panel.grid.minor = element_blank(),
-          panel.grid.major = element_line(size = 0.1),
+          panel.grid.major = element_line(linewidth = 0.1),
           axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11)) +
     xlab(label = '\nyear\n') +
