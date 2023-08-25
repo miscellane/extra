7d71be7b55a430ba9bf83bf9c8d0415f7f579f7a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-25 18:17:26 +0100

    Update graphs/R/diagrams/Aggregates.R

--- graphs/R/diagrams/Aggregates.R
+++ graphs/R/diagrams/Aggregates.R
@@ -4,6 +4,8 @@ Aggregates <- function () {
   T <- read.csv(file = file.path(getwd(), 'warehouse', 'expenditure', 'metrics', 'aggregates.csv'))
   str(object = T)
 
+  T <- dplyr::rename(T, annual_segment_rate = 'annual_segment_.', series_delta_rate = 'series_delta_.')
+  str(object = T)
 
-
+  
 }
