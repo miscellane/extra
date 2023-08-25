de2d5650255c0f7db0808e56699b875e15a9f0ec
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-25 21:04:48 +0100

    Add src/metrics/aggregates/structuring.py

---
+++ src/metrics/aggregates/structuring.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class Structuring:
+    """
+
+    This class structures the output of src.metrics.aggregates.architecture for graphing purposes
+    """
+
+    def __init__(self):
+        pass
+
+    def exc(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        :return:
+        """
