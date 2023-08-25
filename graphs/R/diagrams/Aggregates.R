72315fa062cf1a101640281546ddd46bf500092b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-25 23:05:58 +0100

    Update graphs/R/diagrams/Aggregates.R

--- graphs/R/diagrams/Aggregates.R
+++ graphs/R/diagrams/Aggregates.R
@@ -12,7 +12,7 @@ Aggregates <- function () {
     dplyr::select(segment_code, year, annual_segment_total, annual_segment_rate, series_delta_rate) %>%
     tidyr::gather(key = 'partition', value = 'value', -c('segment_code', 'year')) %>%
     ggplot(mapping = aes(x = year, y = value, colour = segment_code)) +
-    geom_point(alpha = 0.25) +
+    geom_point(alpha = 0.25, na.rm = TRUE) +
     facet_wrap(~partition, nrow = 3, ncol = 1, scales = 'free_y') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 2, units = 'lines'),
