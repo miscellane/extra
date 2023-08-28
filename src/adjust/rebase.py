50d78b71d65b1373520390507645c620aac0e223
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-29 00:01:52 +0100

    Update src/adjust/rebase.py

--- src/adjust/rebase.py
+++ src/adjust/rebase.py
@@ -66,7 +66,7 @@ class Rebase:
         data = blob.copy()[['epoch', 'rebase']]
         data.rename(columns={'epoch': 'x', 'rebase': 'y'}, inplace=True)
         dictionary = {'name': 'deflator',
-                      'key': excerpt,
+                      'attribute': excerpt,
                       'description': 'Deflator Series',
                       'data': data.to_dict(orient='records')}
         return src.functions.objects.Objects().write(
