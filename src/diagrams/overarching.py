5f734938288a01b3177166efe85412c5931fc5ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-02 09:38:17 +0100

    Update src/diagrams/overarching.py

--- src/diagrams/overarching.py
+++ src/diagrams/overarching.py
@@ -1,3 +1,6 @@
+"""
+overarching.py
+"""
 import logging
 import os
 import sys
@@ -6,19 +9,29 @@ import pandas as pd
 
 
 def main():
+    """
+    Entry point
+    :return:
+    """
 
+    # The data set of top government divisions/segments
     data = src.functions.streams.Streams().read(
         uri=os.path.join(root, 'data', 'expenditure_transaction_segments.csv'),
         usecols=['id', 'parent', 'segment_description', 'segment_code'])
     data.rename(columns={'segment_description': 'name', 'segment_code': 'identifier'}, inplace=True)
+
+    # For directed acyclic graph purposes, append a collapsed field.  If a node is collapsed, its
+    # children are hidden by default
     data.loc[:, 'collapsed'] = None
 
+    # The parent node's details
     node = pd.DataFrame(data={'id': 'central', 'parent': None, 'name': 'Central Government',
                               'identifier': '_T', 'collapsed': True})
-
     data = pd.concat([node, data], axis=0, ignore_index=True)
 
-    src.functions.objects.Objects().write(nodes=data.to_dict(orient='tight'), path='')
+    # Save
+    message = src.functions.objects.Objects().write(nodes=data.to_dict(orient='tight'), path='')
+    logger.info(message)
 
 
 if __name__ == '__main__':
