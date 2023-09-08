e83def01ac36c75537d2869816a6a4cfc65ef1fa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-08 20:58:20 +0100

    Add graphs/assets/js/delta.js

---
+++ graphs/assets/js/delta.js
@@ -0,0 +1,15 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $("#option_selector");
+
+var url = document.getElementById("delta").getAttribute("url");
+var url = document.getElementById("delta").getAttribute("variable");
+
+// Build data selection menu
+$.getJSON(url, function(data) {
+
+	for (var k = 0; k < data.length; k += 1) {
+
+	}
+
+});
