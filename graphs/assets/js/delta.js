82aff67ee52a0ff81f174afa12e4a593d5c0cf47
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-08 21:22:59 +0100

    Update graphs/assets/js/delta.js

--- graphs/assets/js/delta.js
+++ graphs/assets/js/delta.js
@@ -5,11 +5,29 @@ var dropdown = $("#option_selector");
 var url = document.getElementById("delta").getAttribute("url");
 var url = document.getElementById("delta").getAttribute("variable");
 
+
+
 // Build data selection menu
-$.getJSON(url, function(data) {
+$.getJSON(url, function (source) {
 
-	for (var k = 0; k < data.length; k += 1) {
+		// Partition
+    let partitions = source.partitions;
+    var pa = partitions.indexOf('series_delta_%');
 
-	}
+    // Data parts of ...
+    let data = source.data[pa];
+
+		// Menu
+    for (var k = 0; k < (data.length - 1); k += 1) {
+        dropdown.append($("<option></option>").attr("value", data[k].name).text(data[k].description));
+    }
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
 
 });
+
