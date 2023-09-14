f9a89c6e1dfea393c6d93c34bfe0ef4e950e940d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-14 22:56:19 +0100

    Update graphs/assets/js/delta.js

--- graphs/assets/js/delta.js
+++ graphs/assets/js/delta.js
@@ -120,7 +120,7 @@ function generateChart(fileNameKey) {
             },
 
             legend: {
-                enabled: true,
+                enabled: false,
                 x: 15
             },
 
