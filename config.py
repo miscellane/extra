d39e24ce0a2158a9342dfe30ee5e07fac52e7203
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-18 17:05:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,13 +20,13 @@ class Config:
         # quarterly national accounts (qna)
         Expenditure = collections.namedtuple(
             typename='Expenditure',
-            field_names=['source', 'aggregates', 'years', 'destination', 'datapath', 'unavailable'])
+            field_names=['source', 'aggregates', 'years', 'initial_', 'revalued_', 'unavailable'])
         start = 1995
         stop = 2021
         self.expenditure = Expenditure(
             source=os.path.join(os.getcwd(), 'data', 'expenditure', 'esa_table_11_central_government.xls'),
             aggregates=['_T', 'GF01', 'GF02', 'GF03', 'GF04', 'GF05', 'GF06', 'GF07', 'GF08', 'GF09', 'GF10'],
             years=np.linspace(start=start, stop=stop, num=stop - start + 1, dtype=int),
-            destination=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'initial'),
-            datapath=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'revalued'),
+            initial_=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'data', 'initial'),
+            revalued_=os.path.join(os.getcwd(), 'warehouse', 'expenditure', 'data', 'revalued'),
             unavailable=['D4.2', 'D4.4', 'D7.2', 'D7.4', 'D9.2', 'D9.4', 'P31', 'P32'])
