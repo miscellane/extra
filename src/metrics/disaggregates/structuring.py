d75315f5ee8b1971dcba45c8843f1c1db91bf73b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-24 18:23:48 +0100

    Update src/metrics/disaggregates/structuring.py: Development in progress.

--- src/metrics/disaggregates/structuring.py
+++ src/metrics/disaggregates/structuring.py
@@ -16,3 +16,13 @@ class Structuring:
         self.__storage = storage
 
         self.__partitions = ['OTE', 'annual_segment_%', 'series_delta_%']
+
+    def __persist(self, dictionary: any, path: str) -> str:
+        """
+
+        :param dictionary:
+        :param path:
+        :return:
+        """
+
+        return src.functions.objects.Objects().write(nodes=dictionary, path=path)
