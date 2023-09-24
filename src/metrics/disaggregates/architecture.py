8f997c3f8e1be19c9d6645dbfcad7ca4901d6484
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-24 18:44:36 +0100

    Update src/metrics/disaggregates/architecture.py: In Progress

--- src/metrics/disaggregates/architecture.py
+++ src/metrics/disaggregates/architecture.py
@@ -27,12 +27,6 @@ class Architecture:
         # The fields in focus: The overall government expenditure per segment code is recorded in field <OTE>
         self.__usecols = ['code', 'OTE', 'segment_code', 'year', 'epoch']
 
-        # logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
-
     def __read(self) -> pd.DataFrame:
         """
 
