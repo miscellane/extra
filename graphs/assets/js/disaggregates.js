07b8e7f0192ba32438b98c326202930400d628c6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-10-12 23:34:50 +0100

    Update graphs/assets/js/disaggregates.js: In Progress

--- graphs/assets/js/disaggregates.js
+++ graphs/assets/js/disaggregates.js
@@ -35,6 +35,7 @@ dropdown.on('change', function (e) {
     generateChart(valueSelected, optionSelected);
 });
 
+
 // Draw
 function generateChart(fileNameKey, fileNameValue){
 
@@ -42,183 +43,146 @@ function generateChart(fileNameKey, fileNameValue){
 	$.getJSON('https://raw.githubusercontent.com/thirdreading/investments/develop/warehouse/expenditure/metrics/disaggregates/' + fileNameKey + '.json', function (source){
 
 
-		// Definitions
-        var total = [],
-            percentage = [],
-            groupingUnits = [[
-                'year',   // unit name
-                [1]      // allowed multiples
-            ]],
-            i = 0;
-
-
         // Partition
         let partitions = source.partitions;
         var to = partitions.indexOf('OTE');
 
 
+        // Definitions
+        var seriesOption = [];
+
+
         // Splits
         for (var i = 0; i < source.data[to].length; i += 1) {
-            total[i] = {
+            seriesOption[i] = {
                 name: source.data[to][i].description,
-                visible: visible,
                 data: source.data[to][i].data
             };
         }
 
 
 		// Graphing
-		Highcharts.stockChart('container0003', {
+		Highcharts.setOptions({
+		  lang: {
+		      thousandsSep: ","
+		  }
+		});
 
 
-	        // Selector
-	        rangeSelector: {
-	            buttonPosition: {
-	            x: 0,
-	            y: 0
-		        },
-		        buttons: [
-		          {
-		            type: 'year',
-		            count: 5,
-		            text: '5y',
-		            title: 'View 5 years',
-		            dataGrouping: {
-		              units: [['year', [1]]]
-		            }
-		          }, {
-		           type: 'year',
-		           count: 10,
-		           text: '10y',
-		           title: 'View 10 years',
-		           dataGrouping: {
-		             units: [['year', [1]]]
-		           }
-		          }, {
-		            type: 'all',
-		            text: 'All',
-		            title: 'View all'
-		          }
-		        ],
-		        floating: false,
-		        inputDateFormat: '%Y',
-		        inputEnabled: true,
-		        inputPosition: {
-		            x: 0,
-		            y: 0
-		        },
-		        selected: 5,
-		        verticalAlign: 'top'
-		    },
+		// Graphing
+		Highcharts.chart('container0005', {
 
+	        chart: {
+				type: "spline",
+				zoomType: "xy",
+				marginTop: 85,
+				marginBottom: 175,
+				marginRight: 5,
+				height: 445,
+				width: 405,
+			},
 
-		    // Chart
-		    chart: {
-	            zoomType: 'x',
-	            type: 'spline',
-	            height: 635,
-	            width: 395
-	        },
+			title: {
+				text: 'United Kingdom<br>Annual Central Government Expenditure',
+				x: 0,
+				y: 5,
+				style: {
+				  fontSize: '15px'
+				}
+			},
 
+			subtitle: {
+				text: fileNameValue,
+				x: 0,
+				y: 45,
+				style: {
+				  fontStyle: 'italic',
+				  fontSize: '13px',
+				  fontWeight: 'normal',
+				  color: 'grey',
+				  width: '80px'
+				}
+			},
 
-			// Title
-	        title: {
-	            text: 'Expenditure'
-	        },
+			credits: {
+				enabled: false
+			},
 
+			legend: {
+				enabled: true,
+				layout: 'horizontal',
+				align: 'center',
+				itemStyle: {
+					fontSize: '11px',
+					width: '120px',
+					textOverflow: 'ellipsis'
+				},
+				verticalAlign: 'bottom',
+				margin: 20,
+				itemMarginTop: 2,
+				itemMarginBottom: 2,
+				x: 5.5,
+				y: 0,
+				floating: false
+			},
 
-			// Subtitle
-	        subtitle: {
-	            text: '<p>Annual Central Government Expenditure</p> <br/> ' +
-	                '<p><b>...</b></p>'
-	        },
+			xAxis: {
+				title: {
+					text: 'Year'
+				},
+				maxPadding: 0.1,
+				gridLineWidth: 1
+			},
 
+			yAxis: {
+			title: {
+				text: "Expense<br>(million pounds)"
+			},
+			maxPadding: 0.05,
+			min: 0,
+			endOnTick: false
+			},
 
-	        // Credits
-	        credits: {
-	            enabled: true
-	        },
+			exporting: {
+			  buttons: {
+			      contextButton: {
+			          menuItems: ["viewFullscreen", "printChart", "separator",
+			              "downloadPNG", "downloadJPEG", "downloadPDF", "downloadSVG", "separator",
+			              "downloadXLS", "downloadCSV"]
+			      }
+			  }
+			},
 
+			tooltip: {
+				shared: true,
+				headerFormat: '<p><span style="font-size: 13px; color:#aab597">\u25CF {point.x:.0f}</span></p>',
+				pointFormat: '<br/><p><br/>' +
+				  '<span style="color:{point.color}">{series.name}</span>: {point.y:,.2f} (m£)<br/></p>' ,
+				style: {
+					fontSize: "11px"
+				}
+			},
 
-	        // Legend
-	        legend: {
-	            enabled: true,
-	            layout: 'horizontal',
-	            align: 'center',
-	            itemStyle: {
-	                fontSize: '11px',
-	                width: '150px',
-	                textOverflow: 'ellipsis'
-	            },
-	            verticalAlign: 'bottom',
-	            margin: 25,
-	            width: 560,
-	            x: 85
-	        },
-
-
-	        // Exporting
-	        exporting: {
-	            buttons: {
-	                contextButton: {
-	                    menuItems: [ 'viewFullscreen', 'printChart', 'separator',
-	                        'downloadPNG', 'downloadJPEG', 'downloadPDF', 'downloadSVG' , 'separator',
-	                        'downloadXLS', 'downloadCSV']
-	                }
-	            }
-	        },
-
-
-	        // Axes
-	        xAxis: {
-	          gridLineWidth: 0.5
-	        },
-
-	        yAxis: {
-	             labels: {
-	                 align: 'left',
-	                 x: 5
-	             },
-	             title: {
-	                 text: 'Annual Transaction<br>Total (m£)',
-	                 align: 'middle',
-	                 margin: 5,
-	                 x: 9
-	             },
-	             min: 0,
-	             height: '42.5%',
-	             lineWidth: 0.05,
-	             resize: {
-	                 enabled: true
-	            }
-	        },
-
-
-	        // Plot Options
-	        plotOptions: {
-	            series: {
-	                marker: {
-	                    enabled: true,
-	                    radius: 1,
-	                    symbol: 'circle'
-	                },
-	                lineWidth: 0.5,
-	                turboThreshold: 4000,
-	                dataGrouping: {
-	                  units: groupingUnits
-	                }
-	            }
-
-	        },
-
+			plotOptions: {
+				series: {
+					marker: {
+						enabled: true,
+					    radius: 1
+					},
+					lineWidth: 0.5,
+					dataLabels: {
+					    enabled: false
+					},
+					turboThreshold: 4000
+				}
+			},
 
 			// Colours
 	        colors: ['#722f37', '#a000c8', '#800000', '#FFA500', '#6b8e23',
 	                 '#000000', '#999090', '#8080ff', '#ff9966', '#214949'],
 
-
 	        // Series
-	        series: total
+	        series: seriesOption
 
 
         });
