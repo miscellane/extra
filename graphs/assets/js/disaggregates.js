d390f33262cfaf35283c94a77ba8de2423068ad3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-10-12 20:08:08 +0100

    Update graphs/assets/js/disaggregates.js: In Progress

---
+++ graphs/assets/js/disaggregates.js
@@ -0,0 +1,41 @@
+// Declarations
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = 'https://raw.githubusercontent.com/thirdreading/investments/develop/warehouse/expenditure/metrics/disaggregates/menu.json';
+
+
+// Menu data
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.name).text(entry.desc));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption, optionSelected);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected, optionSelected);
+});
+
+// Draw
+function generateChart(fileNameKey, fileNameValue){
+
+}
